5f66693c46be6fd7c77d73eade1c7edbd5a6c3d8
posiyans <posiyans@gmail.com>
2019-04-09 17:51:43 +0300

    Update soft_bat.bat

--- windows/chocolatey/soft_bat.bat
+++ windows/chocolatey/soft_bat.bat
@@ -1,13 +1,17 @@
 rem download and run
 rem curl https://raw.githubusercontent.com/posiyans/scrips/master/windows/chocolatey/soft_bat.bat --output %TEMP%\start.bat && %TEMP%\start.bat
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 @echo off
+set /P AllowInsecureGuestAuth='Allow Insecure Guest Auth???'
+IF "%AllowInsecureGuestAuth%" == "y" (
+    set /P localbat='Start local.bat??'
+)
 set /P officeStandart='install word,excel,powerpoint and outlook??'
 IF NOT "%officeStandart%" == "y" (
   set /P office='install word,excel,powerpoint??'
 )
 set /P all='install adoreader, Foxitreader, chocolateygui, radmin-server?!'
+
 IF "%all%" == "y" (
   set adobe=y
   set foxit=y
@@ -20,6 +24,10 @@ IF "%all%" == "y" (
   set /P radmin='install radmin-server?'
 )
 pause
+IF "%AllowInsecureGuestAuth%" == "y" (
+    reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\LanmanWorkstation /v AllowInsecureGuestAuth /t reg_dword /d 00000001 /f
+)
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco install classic-shell -y
 choco install firefox -y
 choco install winrar -y
@@ -42,5 +50,9 @@ IF "%office%" == "y" (
 )
 IF "%officeStandart%" == "y" (
   choco install microsoft-office-deployment -Parameters "/64bit /DisableUpdate:TRUE /Language:ru-ru /Product:Word2019Retail,Excel2019Retail,PowerPoint2019Retail,Outlook2019Retail" -y
+)
+
+IF "%localbat%" == "y" (
+    \\files\it\soft\install\start.bat
 )
 pause
